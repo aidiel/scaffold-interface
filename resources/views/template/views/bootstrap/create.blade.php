476e610b73fec7a1cdf8abd8068bf979da506c44
aidiel <aidiel.effendy@gmail.com>
2017-05-07 12:34:05 +0800

    Add breadcrumb & container

--- resources/views/template/views/bootstrap/create.blade.php
+++ resources/views/template/views/bootstrap/create.blade.php
@@ -1,37 +1,53 @@
 @@extends('layouts.main')
 @@section('title','Create')
 @@section('content')
-<section class="content">
-    <div class="box box-primary">
-        <div class="box-header">
-            <h3>Create New {{ucwords(str_replace('_', ' ', $parser->singular()))}}</h3>
-        </div>
-        <div class="box-body">
-            <form method = 'get' action = '@{!!url("{{$parser->singular()}}")!!}'>
-                <button class = 'btn btn-danger'>Show All {{ucwords(str_replace('_', ' ', $parser->plural()))}}</button>
-            </form>
-            <br>
-            <form method = 'POST' action = '@{!!url("{{$parser->singular()}}")!!}'>
-                <input type = 'hidden' name = '_token' value = '@{{Session::token()}}'>
-                @foreach($dataSystem->dataScaffold('v') as $value)
-                <div class="form-group">
-                    <label for="{{$value}}">{{ucwords(str_replace('_', ' ', $value))}}</label>
-                    <input id="{{$value}}" name = "{{$value}}" type="text" class="form-control" placeholder = "{{ucwords(str_replace('_', ' ', $value))}}">
+<!-- breadcrumbs -->
+<div class="container">
+    <section class="content-header">
+        <ol class="breadcrumb">
+            <li><a href="{{ url('/') }}"><i class="fa fa-home"></i> Home</a></li>
+            <li><a href="@{!!url('{{$parser->singular()}}')!!}"><i class="fa fa-home"></i> All {{ucwords(str_replace('_', ' ', $parser->plural()))}}</a></li>
+            <li class="active"><i>Create New {{ucwords(str_replace('_', ' ', $parser->singular()))}}</i></li>
+        </ol>
+    </section>
+</div>
+<!-- /breadcrumbs -->
+
+<div id="pageContent">
+    <div class="container">
+        <section class="content">
+            <div class="box box-primary">
+                <div class="box-header">
+                    <h3>Create New {{ucwords(str_replace('_', ' ', $parser->singular()))}}</h3>
                 </div>
-                @endforeach
-                @foreach($dataSystem->getForeignKeys() as $key)
-                <div class="form-group">
-                    <label>{{$key}} Select</label>
-                    <select name = '{{lcfirst(str_singular($key))}}_id' class = 'form-control'>
-                        @@foreach(${{str_plural($key)}} as $key => $value)
-                        <option value="@{{$key}}">@{{$value}}</option>
-                        @@endforeach
-                    </select>
+                <div class="box-body">
+                    <form method = 'get' action = '@{!!url("{{$parser->singular()}}")!!}'>
+                        <button class = 'btn btn-danger'>Show All {{ucwords(str_replace('_', ' ', $parser->plural()))}}</button>
+                    </form>
+                    <br>
+                    <form method = 'POST' action = '@{!!url("{{$parser->singular()}}")!!}'>
+                        <input type = 'hidden' name = '_token' value = '@{{Session::token()}}'>
+                        @foreach($dataSystem->dataScaffold('v') as $value)
+                        <div class="form-group">
+                            <label for="{{$value}}">{{ucwords(str_replace('_', ' ', $value))}}</label>
+                            <input id="{{$value}}" name = "{{$value}}" type="text" class="form-control" placeholder = "{{ucwords(str_replace('_', ' ', $value))}}">
+                        </div>
+                        @endforeach
+                        @foreach($dataSystem->getForeignKeys() as $key)
+                        <div class="form-group">
+                            <label>{{$key}} Select</label>
+                            <select name = '{{lcfirst(str_singular($key))}}_id' class = 'form-control'>
+                                @@foreach(${{str_plural($key)}} as $key => $value)
+                                <option value="@{{$key}}">@{{$value}}</option>
+                                @@endforeach
+                            </select>
+                        </div>
+                        @endforeach
+                        <button class = 'btn btn-primary' type ='submit'>Create</button>
+                    </form>
                 </div>
-                @endforeach
-                <button class = 'btn btn-primary' type ='submit'>Create</button>
-            </form>
-        </div>
+            </div>
+        </section>
     </div>
-</section>
+</div>
 @@endsection
--- resources/views/template/views/bootstrap/show.blade.php
+++ resources/views/template/views/bootstrap/show.blade.php
@@ -1,46 +1,62 @@
 @@extends('layouts.main')
 @@section('title','Show')
 @@section('content')
-<section class="content">
-    <div class="box box-primary">
-        <div class="box-header">
-            <h3>Show {{ucwords(str_replace('_', ' ', $parser->singular()))}}</h3>
-        </div>
-        <div class="box-body">
-    
-    <form method = 'get' action = '@{!!url("{{$parser->singular()}}")!!}'>
-        <button class = 'btn btn-primary'>Show All {{ucwords(str_replace('_', ' ', $parser->plural()))}}</button>
-    </form>
-    <br>
-    <table class = 'table table-bordered'>
-        <thead>
-            <th>Key</th>
-            <th>Value</th>
-        </thead>
-        <tbody>
-            @foreach($dataSystem->dataScaffold('v') as $value)
-            <tr>
-                <td>
-                    <b><i>{{$value}} : </i></b>
-                </td>
-                <td>@{!!${{$parser->singular()}}->{{$value}}!!}</td>
-            </tr>
-            @endforeach
-            @if($dataSystem->getRelationAttributes() != null)
-            @foreach($dataSystem->getRelationAttributes() as $key=>$value)
-            @foreach($value as $key1 => $value1)
-            <tr>
-                <td>
-                    <b><i>{{$value1}} : </i></b>
-                </td>
-                <td>@{!!${{$parser->singular()}}->{{str_singular($key)}}->{{$value1}}!!}</td>
-            </tr>
-            @endforeach
-            @endforeach
-            @endif
-        </tbody>
-    </table>
+<!-- breadcrumbs -->
+<div class="container">
+    <section class="content-header">
+        <ol class="breadcrumb">
+            <li><a href="{{ url('/') }}"><i class="fa fa-home"></i> Home</a></li>
+            <li><a href="@{!!url('{{$parser->singular()}}')!!}"><i class="fa fa-home"></i> All {{ucwords(str_replace('_', ' ', $parser->plural()))}}</a></li>
+            <li class="active"><i>Show {{ucwords(str_replace('_', ' ', $parser->singular()))}}</i></li>
+        </ol>
+    </section>
+</div>
+<!-- /breadcrumbs -->
+
+<div id="pageContent">
+    <div class="container">
+        <section class="content">
+            <div class="box box-primary">
+                <div class="box-header">
+                    <h3>Show {{ucwords(str_replace('_', ' ', $parser->singular()))}}</h3>
+                </div>
+                <div class="box-body">
+
+                    <form method = 'get' action = '@{!!url("{{$parser->singular()}}")!!}'>
+                        <button class = 'btn btn-primary'>Show All {{ucwords(str_replace('_', ' ', $parser->plural()))}}</button>
+                    </form>
+                    <br>
+                    <table class = 'table table-bordered'>
+                        <thead>
+                        <th>Key</th>
+                        <th>Value</th>
+                        </thead>
+                        <tbody>
+                            @foreach($dataSystem->dataScaffold('v') as $value)
+                            <tr>
+                                <td>
+                                    <b><i>{{$value}} : </i></b>
+                                </td>
+                                <td>@{!!${{$parser->singular()}}->{{$value}}!!}</td>
+                            </tr>
+                            @endforeach
+                            @if($dataSystem->getRelationAttributes() != null)
+                            @foreach($dataSystem->getRelationAttributes() as $key=>$value)
+                            @foreach($value as $key1 => $value1)
+                            <tr>
+                                <td>
+                                    <b><i>{{$value1}} : </i></b>
+                                </td>
+                                <td>@{!!${{$parser->singular()}}->{{str_singular($key)}}->{{$value1}}!!}</td>
+                            </tr>
+                            @endforeach
+                            @endforeach
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
     </div>
-        </div>
-</section>
+</div>
 @@endsection
